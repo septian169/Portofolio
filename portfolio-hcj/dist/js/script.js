1f4a17f73d23698b5d717289248b682d4b1dfcc5
septian169 <saputraputraa219481@gmail.com>
2025-10-23 13:47:50 +0700

    Delete portfolio-hcj directory

--- portfolio-hcj/dist/js/script.js
+++
@@ -1,9 +0,0 @@
-const scrollUp = document.querySelector(".scroll-up");
-
-window.onscroll = () => {
-  if (window.scrollY > 500) {
-    scrollUp.classList.add("scroll-active");
-  } else {
-    scrollUp.classList.remove("scroll-active");
-  }
-};
